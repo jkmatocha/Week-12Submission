585a60f3ffe1326f387a326f29374029950b0cd4
John Matocha <jkmatocha@gmail.com>
2019-11-19 10:39:19 -0700

    fourth

--- Week12.py
+++ Week12.py
@@ -33,3 +33,21 @@ def UpdateString(string1, string2, int1):
          
             
     print(newstring)
+
+#   ScoreFinder
+#   John Matocha
+#   ​CSCI 102 – Section A
+#   Week 12 - Part A
+def ScoreFinder(play, score, name):
+    index = -1
+    name = name.lower()
+    print(name)
+    for i in range(len(play)):
+        play[i] = play[i].lower()
+    for i in range(len(play)):
+        if name == play[i]:
+            index = i
+    if index >= 0:
+        print('OUTPUT', play[index], 'got a score of', score[index])
+    else:
+        print('OUTPUT player not found')
