704bf43c56bc3be4ad0a9ff0462d1596857fcf68
John Matocha <jkmatocha@gmail.com>
2019-11-19 10:38:14 -0700

    third function

--- Week12.py
+++ Week12.py
@@ -17,3 +17,19 @@ def LoadFile(file):
         for k in list1:
             final.append()
     return final
+
+#   UpdatedString
+#   John Matocha
+#   ​CSCI 102 – Section A
+#   Week 12 - Part A
+def UpdateString(string1, string2, int1):
+    newstring = ''
+    
+    for i in range(len(string1)):
+        if i == int1:
+            newstring+=string2
+        else:
+            newstring += string1[i]
+         
+            
+    print(newstring)
