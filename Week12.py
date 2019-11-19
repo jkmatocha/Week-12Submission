d1594c2775891cc4efc29a74c6f72bc237e7f2db
John Matocha <jkmatocha@gmail.com>
2019-11-19 10:36:53 -0700

    second function

--- Week12.py
+++ Week12.py
@@ -4,3 +4,16 @@
 #   Week 12 - Part A
 def PrintOutput(string):
     print('OUTPUT', string)
+
+#   LoadFile
+#   John Matocha
+#   ​CSCI 102 – Section A
+#   Week 12 - Part A
+def LoadFile(file):
+    lines = open(file, 'r').readlines()
+    final = []
+    for i in lines:
+        list1 = i.split()
+        for k in list1:
+            final.append()
+    return final
